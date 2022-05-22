12f4d81367b653b75d8595bf37c9048f3f1f632e
javohir101dev <uralovjavohir25@gmail.com>
2022-05-22 19:32:01 +0500

    Auditing configuration

---
+++ src/main/java/emailauditing/config/AuditingConfigId.java
@@ -0,0 +1,25 @@
+package emailauditing.config;
+
+import emailauditing.entity.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class AuditingConfigId implements AuditorAware<UUID> /*id type of user*/ {
+    @Override
+    public Optional getCurrentAuditor() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null
+        && authentication.isAuthenticated()
+        && !authentication.getPrincipal().equals("anonymousUser")){
+            User user =(User) authentication.getPrincipal();
+            return Optional.of(user.getId());
+        }
+
+        return Optional.empty();
+    }
+}
--- src/main/java/emailauditing/entity/Product.java
+++ src/main/java/emailauditing/entity/Product.java
@@ -8,10 +8,13 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.awt.event.AWTEventListener;
 import java.sql.Timestamp;
 import java.util.UUID;
 
@@ -19,6 +22,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Product {
 
     @Id
